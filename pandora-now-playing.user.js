81f1c3ca5881c3441111548c1976fc8ae60945c4
Ben Hutchison <ben@aldaviva.com>
2019-06-21 03:31:28 -0700

    Added Pandora Now Playing script

---
+++ pandora-now-playing.user.js
@@ -0,0 +1,31 @@
+// ==UserScript==
+// @name         Pandora Now Playing
+// @namespace    https://aldaviva.com/userscripts/pandora-now-playing
+// @version      0.0.1
+// @description  Show now playing artist and song title in Pandora document title
+// @author       Ben Hutchison
+// @match        https://www.pandora.com/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    setInterval(function(){
+        var songTitleEl = document.getElementsByClassName("Tuner__Audio__TrackDetail__title");
+        var artistEl = document.getElementsByClassName("Tuner__Audio__TrackDetail__artist");
+
+        var newDocumentTitle;
+        if(songTitleEl.length && artistEl.length){
+            var originalDocumentTitle = document.querySelector("head meta[name='description'][data-react-helmet='true']").content;
+            var nowPlayingPhrase = artistEl[0].textContent + " - " + songTitleEl[0].textContent;
+            newDocumentTitle = nowPlayingPhrase + " - " + originalDocumentTitle;
+        } else {
+            newDocumentTitle = originalDocumentTitle;
+        }
+
+        document.title = newDocumentTitle;
+    }, 1000);
+
+    console.info("Pandora Now Playing user script initialized");
+})();
