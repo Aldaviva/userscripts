59f88f0ad252245b44502d3ecfcdc28ff6382654
Ben Hutchison <ben@aldaviva.com>
2019-10-03 22:03:04 -0700

    When now playing information is not available, such as the log in page, show the page's original title, instead of "undefined" or the meta description text

--- pandora-now-playing.user.js
+++ pandora-now-playing.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Pandora Now Playing
 // @namespace    https://aldaviva.com/userscripts/pandora-now-playing
-// @version      0.0.1
+// @version      0.0.2
 // @description  Show now playing artist and song title in Pandora document title
 // @author       Ben Hutchison
 // @match        https://www.pandora.com/*
@@ -11,13 +11,15 @@
 (function() {
     'use strict';
 
+    var originalDocumentTitle = document.title;
+
     setInterval(function(){
         var songTitleEl = document.getElementsByClassName("Tuner__Audio__TrackDetail__title");
         var artistEl = document.getElementsByClassName("Tuner__Audio__TrackDetail__artist");
+        //var originalDocumentTitle = document.querySelector("head meta[name='description'][data-react-helmet='true']").content;
 
         var newDocumentTitle;
         if(songTitleEl.length && artistEl.length){
-            var originalDocumentTitle = document.querySelector("head meta[name='description'][data-react-helmet='true']").content;
             var nowPlayingPhrase = artistEl[0].textContent + " - " + songTitleEl[0].textContent;
             newDocumentTitle = nowPlayingPhrase + " - " + originalDocumentTitle;
         } else {
