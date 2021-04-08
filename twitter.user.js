83d82cb8cb565d4017f7a785b25b6e3d9ed0844a
Ben Hutchison <ben@aldaviva.com>
2021-04-07 23:16:26 -0700

    Update Twitter userscript to conditionally block only calls to Date.now() that cause timeline updates or page reloads. Other calls should be left alone, to fix relative timestamp rendering as well as data loss when creating multi-tweet thread from What's Happening box and opening modal composer.

--- twitter.user.js
+++ twitter.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         Twitter
 // @namespace    https://aldaviva.com/userscripts/twitter
-// @version      1.1.0
-// @description  Always show Latest Tweets First on the home timeline, instead of Top Tweets First. Ignore unread notifications, but still show unread DMs.
+// @version      1.2.0
+// @description  Always show Latest Tweets First on the home timeline, instead of Top Tweets First. Ignore unread notifications, but still show unread DMs. Don't automatically reload all the time.
 // @author       Ben Hutchison
 // @match        https://twitter.com/*
 // @grant        none
@@ -18,7 +18,9 @@
         setInterval(ensureLatestTweetsFirst, 1 * 60 * 1000);
         ensureLatestTweetsFirst();
 
-        setTimeout(ignoreNotifications, 500); //wait for title el to be created
+        //setTimeout(ignoreNotifications, 500); //wait for title el to be created
+
+        preventAutoReloading();
     }
 
     function ensureLatestTweetsFirst(){
@@ -62,7 +64,7 @@
             const hasUnreadNotifications = titleNotificationCount > directMessageCount;
 
             if(hasUnreadNotifications) {
-                const titlePrefix = directMessageCount > 0 ? "("+directMessageCount+") " : "";
+                const titlePrefix = directMessageCount > 0 ? `(${directMessageCount}) ` : "";
                 const newTitleValue = titleEl.text.replace(titleNotificationCountPattern, titlePrefix);
                 titleEl.text = newTitleValue;
 
@@ -73,6 +75,87 @@
         }
 
         hideUnreadNotifications();
+    }
+
+    function preventAutoReloading(){
+        /* We're looking for a module containing
+          , p = e=>{
+            const t = Date.now();
+            let n = t;
+            const i = document.cookie
+              , o = u(i);
+            let d = !0;
+            const p = e=>{
+                d = e
+            }
+              , h = e=>{
+                const r = Date.now();
+                if ("active" === e) {
+                    if (d) {
+                        const e = r - t
+                          , i = r - n
+                          , o = e >= 576e5 && i >= 12e4 || e >= 864e5;
+                        !s.a.isLocked() && o && window.location.reload(!0)
+                    }
+                    m()
+                } else
+                    n = r
+            }
+        */
+        const reloaderMethodPattern = /=Date\.now\(\).*?\b(?<methodName>\w{1,3})=\w{1,3}=>{const \w{1,3}=Date\.now\(\).*?window\.location\.reload\(/gm;
+        const originalDateNow = Date.now;
+        const reloaderMethodName = findReloaderMethodName();
+
+        Object.defineProperty(Date, "now", {
+            value: function() {
+                const shouldReturnFakeResult = isExecutingReloaderMethod();
+                //if(shouldReturnFakeResult){
+                //    console.debug("Returning Date.now() = 0");
+                //}
+                return shouldReturnFakeResult ? 0 : originalDateNow();
+            },
+            writable: false,
+            configurable: false
+        });
+
+        function findReloaderMethodName(){
+            for(let bucket of window.webpackJsonp){
+                for(let moduleName in bucket[1]){
+                    const module = bucket[1][moduleName];
+                    const moduleSource = module.toString();
+                    const match = reloaderMethodPattern.exec(moduleSource);
+                    if(match !== null){
+                        //console.debug("found reloader in module "+moduleName);
+                        //console.debug(moduleSource);
+                        return match.groups.methodName;
+                    }
+                }
+            }
+            console.warn("Could not find method that reloads the page");
+            return null;
+        }
+
+        function isExecutingReloaderMethod(){
+            const stack = new Error().stack;
+
+            const methodNamesToFind = [
+                reloaderMethodName, //page was focused, reload entire page
+                "Object.fetchTop", //page was focused, update timeline
+                "fetchInitialOrTop", //timeline was scrolled up to top
+                //"ue._fetchInitialOrTop", //always appears with above method
+                //"Object.fetchInitialOrTop" //appears on its own
+            ].filter(x => x !== null);
+
+            //V8-specific stacktrace format, SpiderMonkey has a different format
+            const isMatchingStack = methodNamesToFind.some(methodName => stack.indexOf(`\n    at ${methodName} (`) !== -1);
+
+            /*if(isMatchingStack){
+                console.debug("Found matching invocation: "+stack);
+            } else {
+                console.debug(`Other call to Date.now() at ${performance.timeOrigin + performance.now()}:`, stack);
+            }*/
+            return isMatchingStack;
+        }
     }
 
 })();
