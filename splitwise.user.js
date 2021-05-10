198d58da9dfa3041c3dfc005ba6a2f66934d641c
Ben Hutchison <ben@aldaviva.com>
2021-05-09 19:15:48 -0700

    Allow additional Splitwise expense titles which set the category automatically.

--- splitwise.user.js
+++ splitwise.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         Splitwise
 // @namespace    https://aldaviva.com/userscripts/splitwise
-// @version      0.0.1
-// @description  Disable Splitwise keyboard shortcuts
+// @version      0.0.2
+// @description  Disable Splitwise keyboard shortcuts. Add more automatic categories.
 // @author       Ben Hutchison
 // @match        https://secure.splitwise.com
 // @grant        none
@@ -14,4 +14,17 @@
     'use strict';
 
     window.$.fn.modal.defaults.keyboard = false;
+
+    const originalGuessCategory = window.guess_category;
+    window.App.Models.Expense.prototype.guessCategoryFromDescription = window.guess_category = function(description){
+        if(/\blucky\b/i.test(description)){
+            return getCategoryIdByName("Groceries");
+        } else {
+            return originalGuessCategory(description);
+        }
+    };
+
+    function getCategoryIdByName(name){
+        return window.App.categories.detect(function(category){ return category.get("name") === name; }).id;
+    }
 })();
