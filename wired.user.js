5defb763fb6e88b38cdda8de716a551d25263ca4
Ben Hutchison <ben@aldaviva.com>
2021-11-23 23:45:52 -0800

    Each Wired cookie has a different domain

--- wired.user.js
+++ wired.user.js
@@ -1,11 +1,10 @@
 // ==UserScript==
 // @name         Wired
 // @namespace    https://aldaviva.com/userscripts/wired
-// @version      0.0.1
+// @version      0.0.2
 // @description  Block the free article limit error
 // @author       Ben Hutchison
 // @match        https://www.wired.com/*
-// @icon         https://www.google.com/s2/favicons?domain=wired.com
 // @grant        none
 // @run-at       document-start
 // ==/UserScript==
@@ -13,13 +12,19 @@
 (function() {
     'use strict';
 
-    const cookiesToDelete = ["verso_bucket", "pay_ent_smp"];
+    const cookiesToDelete = [{
+        name: "verso_bucket",
+        domain: "www.wired.com"
+    }, {
+        name: "pay_ent_smp",
+        domain: ".wired.com"
+    }];
 
     cookiesToDelete.forEach(deleteCookie);
 
-    function deleteCookie(cookieName){
-        document.cookie = cookieName + "=a;path=/;expires=Thu, 01 Jan 1970 00:00:00 GMT;secure";
-        console.log("Wired Userscript: Deleted cookie "+cookieName);
+    function deleteCookie(cookie){
+        document.cookie = cookie.name + "=a;path=/;domain=" + cookie.domain + ";expires=Thu, 01 Jan 1970 00:00:00 GMT;secure";
+        console.info("Wired Userscript: Deleted cookie " + cookie.name);
     }
 
 })();
