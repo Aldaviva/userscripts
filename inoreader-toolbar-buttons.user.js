a87c56d6761e7975ae348f1db9b393feea77000e
Ben Hutchison <ben@aldaviva.com>
2021-12-27 21:29:49 -0800

    Inoreader: make feed list gear button open sidebar preferences, as an intermediate step to easily toggling hiding read feeds in the list.

--- inoreader-toolbar-buttons.user.js
+++ inoreader-toolbar-buttons.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Inoreader toolbar buttons
 // @namespace    https://aldaviva.com/userscripts/inoreader-toolbar-buttons
-// @version      0.0.1
+// @version      0.2.0
 // @description  Add useful toolbar buttons to Inoreader
 // @author       Ben Hutchison
 // @match        https://www.inoreader.com/*
@@ -12,15 +12,23 @@
 (function() {
     'use strict';
 
-    var parent = document.getElementById("sb_tp_buttons");
+    addSubscribeAndManageButtons();
 
-    parent.appendChild(createButton("Subscribe", function(event){
-        window.show_add_options();
-    }));
+    showStarredFilterButton();
 
-    parent.appendChild(createButton("Manage", function(event){
-        window.show_dialog('preferences_dialog', { focus_tab: 2 });
-    }));
+    makeFeedGearOpenSidebarPreferences();
+
+    function addSubscribeAndManageButtons(){
+        var parent = document.getElementById("sb_tp_buttons");
+
+        parent.appendChild(createButton("Subscribe", function(event){
+            window.show_add_options();
+        }));
+
+        parent.appendChild(createButton("Manage", function(event){
+            window.show_dialog("preferences_dialog", { focus_tab: 2 });
+        }));
+    }
 
     function createButton(label, onClick){
         var buttonEl = document.createElement("div");
@@ -33,5 +41,32 @@
         buttonEl.appendChild(labelEl);
 
         return buttonEl;
+    }
+
+    function showStarredFilterButton(){
+        var originalToolbarSwitcher = window.inno_toolbar_switcher;
+
+        window.inno_toolbar_switcher = function(id, buttons, switch_function){
+            if(buttons.length < 3){
+                buttons.push({
+                    id: "favorites_cnt_top",
+                    caption: window._js("Starred"),
+                    value: 2,
+                    active: false
+                });
+            }
+
+            return originalToolbarSwitcher(id, buttons, switch_function);
+        };
+    }
+
+    //xajax_save_user_pref("hide_read_feeds", "0");
+    //window.hide_read_feeds
+    function makeFeedGearOpenSidebarPreferences(){
+        document.querySelector(".sf_cog").onclick = function(event){
+            event.preventDefault();
+            console.log("showing sidebar prefs");
+            window.location = "#preferences-interface-tree_pane";
+        };
     }
 })();
