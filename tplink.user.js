53fef0c155409286c9249088bbcf8c8cd1b552e9
Ben Hutchison <ben@aldaviva.com>
2022-12-04 02:23:49 -0800

    TP-Link: stop checking if the main module is loaded after 10 seconds

--- tplink.user.js
+++ tplink.user.js
@@ -1,9 +1,10 @@
 // ==UserScript==
 // @name         Block TP-Link redirections
 // @namespace    https://aldaviva.com/userscripts/tplinkwifiredirect
-// @version      0.0.0
-// @description  TP-Link AX20 v1.20: Don't redirect to tplinkwifi.net every time I try to log in to my router, because the IP address of that domain in DNS is wrong
+// @version      0.0.1
+// @description  TP-Link AX20 v1.20: Don't redirect to tplinkwifi.net every time I try to log in to my router after the last session expired, because the IP address of that domain in DNS is wrong when DNS over HTTPS is enabled and the router can't intercept my DNS requests
 // @author       Ben Hutchison
+// @match        http://192.168.0.1/*
 // @match        http://192.168.1.1/*
 // @grant        none
 // @run-at       document-end
@@ -12,19 +13,23 @@
 (function() {
     'use strict';
 
-    const start = new Date();
     let attempts = 0;
+    const start = new Date();
 
-    let interval = setInterval(() => {
+    const interval = setInterval(() => {
         attempts++;
 
-        let main = window.$.su.moduleManager.query("main");
+        const main = window.$.su.moduleManager.query("main");
         if (main) {
             clearInterval(interval);
 
-            main.onLoginTimeout = () => {};
+            main.onLoginTimeout = () => console.info("Not redirecting to tplinkwifi.net. You're welcome!");
 
             console.info(`Disabled onLoginTimeout so this page stops redirecting to tplinkwifi.net after ${attempts} attempts and ${new Date() - start} ms.`);
+        } else if(attempts >= 500) {
+            // give up after 10 seconds
+            clearInterval(interval);
+            console.warn(`Giving up waiting for the main module to load after ${new Date() - start} ms so redirections to tplinkwifi.net can be blocked.`);
         }
     }, 20);
 })();
