38d83baafe9acba3fce2431635c6edce511afb48
Ben Hutchison <ben@aldaviva.com>
2023-01-22 04:57:41 -0800

    Chronological Twitter: added faster event-based enforcement instead of just polling and magic timeouts. Only enforce on /home.

--- twitter-chronological.user.js
+++ twitter-chronological.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         Chronological Twitter
 // @namespace    https://aldaviva.com/userscripts/twitter/chronological
-// @version      1.3.1
-// @description  Always show Following on the home timeline, instead of For You First.
+// @version      1.3.2
+// @description  Always show Following on the home timeline, instead of For You.
 // @author       Ben Hutchison
 // @match        https://twitter.com/*
 // @grant        none
@@ -13,12 +13,25 @@
     'use strict';
 
     setInterval(ensureLatestTweetsFirst, 1 * 60 * 1000);
-    setTimeout(ensureLatestTweetsFirst, 2 * 1000);
+    ensureLatestTweetsFirst();
+    window.addEventListener("popstate", ensureLatestTweetsFirst);
+
+    const originalPushState = window.history.pushState;
+    window.history.pushState = function(){
+        originalPushState.apply(window.history, arguments);
+        ensureLatestTweetsFirst();
+    };
 
     function ensureLatestTweetsFirst() {
-        const followingTab = document.querySelector("[data-testid='ScrollSnap-List'] :nth-child(2) a[href='/home']");
-        if (followingTab && !isTabActive(followingTab)) {
-            activateTab(followingTab);
+        if(window.location.pathname === "/home"){
+            waitUntilElementsBySelector("[data-testid='ScrollSnap-List'] :nth-child(2) a[href='/home']", 100, +new Date()+5000, (error, elements) => {
+                if(!error) return;
+
+                const followingTab = elements[0];
+                if(!isTabActive(followingTab)){
+                    activateTab(followingTab);
+                }
+            });
         }
     }
 
@@ -28,6 +41,17 @@
 
     function activateTab(tabEl) {
         tabEl.click();
+    }
+
+    function waitUntilElementsBySelector(selector, retryInterval, deadline, callback){
+        const elements = document.querySelectorAll(selector);
+        if(elements.length){
+            callback(null, elements);
+        } else if(new Date() <= deadline) {
+            setTimeout(waitUntilElementsBySelector.bind(null, selector, retryInterval, deadline, callback), retryInterval);
+        } else {
+            callback(new Error("deadline passed and no "+selector+" elements were found"));
+        }
     }
 
 })();
