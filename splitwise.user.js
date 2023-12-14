2dc6d79902e98cc30ec5ad3c4cae3fdf11586e6a
Ben Hutchison <ben@aldaviva.com>
2023-12-13 23:59:27 -0800

    Splitwise: make currency inputs of type numeric, not text, so they disallow commas and other invalid characters

--- splitwise.user.js
+++ splitwise.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         Splitwise
 // @namespace    https://aldaviva.com/userscripts/splitwise
-// @version      0.0.2
-// @description  Disable Splitwise keyboard shortcuts. Add more automatic categories.
+// @version      0.1.0
+// @description  Disable Splitwise keyboard shortcuts. Add more automatic categories. Make currencu inputs numeric.
 // @author       Ben Hutchison
 // @match        https://secure.splitwise.com
 // @grant        none
@@ -13,10 +13,16 @@
 (function() {
     'use strict';
 
+    /**
+     * Don't close the expense entry modal dialog when pressing Esc. This is needed because the dialog box doesn't confirm before losing unsaved changes.
+     */
     window.$.fn.modal.defaults.keyboard = false;
 
+    /**
+     * Guess more categories automatically.
+     */
     const originalGuessCategory = window.guess_category;
-    window.App.Models.Expense.prototype.guessCategoryFromDescription = window.guess_category = function(description){
+    window.App.Models.Expense.prototype.guessCategoryFromDescription = window.guess_category = description => {
         if(/\blucky\b/i.test(description)){
             return getCategoryIdByName("Groceries");
         } else {
@@ -25,6 +31,15 @@
     };
 
     function getCategoryIdByName(name){
-        return window.App.categories.detect(function(category){ return category.get("name") === name; }).id;
+        return window.App.categories.detect(category => category.get("name") === name).id;
     }
+
+    /**
+     * Make currency inputs of type numeric, not text, so they disallow commas and other invalid characters
+     */
+    const originalItemizedItemTemplate = window.JST["backbone/templates/modals/itemized-item"];
+    window.JST["backbone/templates/modals/itemized-item"] = obj => {
+        const originalTemplate = originalItemizedItemTemplate(obj);
+        return originalTemplate.replace('<input type="text" name="amount" class="amount" ', '<input type="number" step="0.01" name="amount" class="amount" ');
+    };
 })();
