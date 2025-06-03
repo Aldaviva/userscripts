28f963feaf01c4e009bd79b34e7b0343516edd57
Ben Hutchison <ben@aldaviva.com>
2025-06-03 06:39:09 -0700

    Freshping: don't change report date range while the previous report is still loading, because then it forgets the check ID and redirects to a different check

--- freshping.user.js
+++ freshping.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Freshping
 // @namespace    https://aldaviva.com/userscripts/freshping
-// @version      0.1.1
+// @version      0.1.2
 // @description  Maximum default report duration
 // @author       Ben Hutchison
 // @match        https://*.freshping.io/*
@@ -20,32 +20,43 @@
      * Element found using
      * (function findRecursively(needle, haystack, path, mark){ mark ||= "__visited"+Math.random()*1e17; try { if(haystack === null || haystack === undefined || haystack[mark]===true){ return null;} haystack[mark]=true; for(var childName in haystack) { if(haystack[childName] === needle) { return path+"."+childName; } } for(var childName in haystack) { if(haystack[childName] === haystack) { continue; } var successfulPath = findRecursively(needle, haystack[childName], path+"."+childName, mark); if(successfulPath !== null) { return successfulPath; }} return null; } catch(e){ return null; }})(window.myReport, $0, "$0")
      */
-    function setReportDurationToMax(){
-        waitUntilElementsBySelector([".App-main > div:first-child:last-child"], 200, new Date() + 5000, (err, els) => {
+    function setReportDurationToMax() {
+        waitUntilElementsBySelector([".App-main > div:first-child:last-child"], 100, new Date() + 5000, (err, els) => {
             if (!err) {
                 const reportsEl = els[0][0];
                 const reports = reportsEl[Object.keys(reportsEl).find(k => k.startsWith("__reactInternalInstance$"))].return.stateNode;
-                if (reports.handleChange) {
-                    const earliestReportDate = new Date();
-                    earliestReportDate.setDate(earliestReportDate.getDate() - 90);
-                    const earliestReportDateString = earliestReportDate.getDate() + " " + monthAbbreviations[earliestReportDate.getMonth()] + " " + earliestReportDate.getFullYear();
-                    if (reports.state.startDate !== earliestReportDateString) {
-                        reports.setState({ startDate: earliestReportDateString });
+                let reportsReadyInterval = setInterval(() => {
+                    if (/*reports.handleTimeChange &&*/ reports.handleChange && reports.state.isLoaded) {
+                        clearInterval(reportsReadyInterval);
+                        const earliestReportDate = new Date();
+                        earliestReportDate.setDate(earliestReportDate.getDate() - 90);
+                        reports.setState({
+                            startDate: earliestReportDate.getDate() + " " + monthAbbreviations[earliestReportDate.getMonth()] + " " + earliestReportDate.getFullYear()
+                        });
+
                         reports.handleChange();
+                        //reports.handleTimeChange();
+                    } else {
+                        console.warn("Reports are not loaded yet", reports);
                     }
-                }
+                }, 100);
             }
         });
     }
 
     window.addEventListener("pushstate", event => {
+        console.log("pushState: url=" + event.url);
         const newUrl = new URL(event.url, window.location.href);
         const newUrlString = newUrl.toString();
         if (oldUrlString !== newUrlString) {
+            console.debug(newUrlString + " is different from " + oldUrlString);
             if (newUrl.pathname == "/reports" && parseInt(newUrl.searchParams.get("check_id"), 10) > 0) {
+                console.debug("Setting report duration to max");
                 setReportDurationToMax();
             }
             oldUrlString = newUrlString;
+        } else {
+            console.debug(newUrlString + " is the same as " + oldUrlString);
         }
     });
 
@@ -55,10 +66,10 @@
         window.dispatchEvent(new PushStateEvent(state, url));
     };
 
-    setTimeout(setReportDurationToMax, 200);
+    setTimeout(setReportDurationToMax, 100);
 
     function waitUntilElementsBySelector(selectors, retryInterval, deadline, callback) {
-        if(typeof selectors.map !== "function"){
+        if (typeof selectors.map !== "function") {
             selectors = [selectors];
         }
 
@@ -74,7 +85,7 @@
     }
 
     class PushStateEvent extends Event {
-        constructor(state, url) {
+        constructor(state, url){
             super("pushstate");
             this.state = state;
             this.url = url;
