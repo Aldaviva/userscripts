f8d7138ba2cfa4c871fb6ef5865e5dfe8a39ab82
Ben Hutchison <ben@aldaviva.com>
2025-08-27 15:54:03 -0700

    Create Passkeys Anywhere: fix 400 errors while creating passkeys on Bitbucket by removing attestation

--- create-passkeys-anywhere.user.js
+++ create-passkeys-anywhere.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Create Passkeys Anywhere
 // @namespace    https://aldaviva.com/userscripts/create-passkeys-anywhere
-// @version      0.0.2
+// @version      0.0.3
 // @description  Let you choose where to store a newly created passkey, on a security key or in the TPM
 // @author       Ben Hutchison
 // @match        https://*/*
@@ -30,17 +30,23 @@
     const authenticatorAttachments = { "anywhere": null, "securityKey": "cross-platform", "tpm": "platform" };
 
     navigatorCredentials.create = function(opts){
+        const authenticatorAttachment = authenticatorAttachments[options.allowedPasskeyCreationStorage];
         const publicKey = opts?.publicKey;
-        if(publicKey){
-            const authenticatorAttachment = authenticatorAttachments[options.allowedPasskeyCreationStorage];
+        if(authenticatorAttachment === undefined){
+            console.warn("Create Passkeys Anywhere: Unknown allowedPasskeyCreationStorage value "+options.allowedPasskeyCreationStorage+", allowed values are anywhere, securityKey, and tpm");
+        } else if(publicKey){
             publicKey.authenticatorSelection ??= {};
             delete publicKey.authenticatorSelection.userVerification;
             if(authenticatorAttachment){
                 publicKey.authenticatorSelection.authenticatorAttachment = authenticatorAttachment;
-            } else if(authenticatorAttachment === null){
-                delete publicKey.authenticatorSelection.authenticatorAttachment;
             } else {
-                console.warn("Create Passkeys Anywhere: Unknown allowedPasskeyCreationStorage value "+options.allowedPasskeyCreationStorage+", allowed values are anywhere, securityKey, and tpm");
+                delete publicKey.authenticatorSelection.authenticatorAttachment;
+            }
+
+            if(publicKey.rp?.id === "bitbucket.org"){
+                // Fixes Bitbucket 400 error from new passkey upload on certain computers (Win11+fingerprint+STM TPM, but neither Win11+PIN+AMD TPM nor Android+fingerprint)
+                // Thanks Erina: https://bsky.app/profile/satragno.bsky.social/post/3lxf5o5djls2u
+                publicKey.attestation = "none";
             }
         }
         return originalCreate.call(navigatorCredentials, opts);
