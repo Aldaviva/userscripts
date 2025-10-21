c5306a1252e0555a124d330d5d87b15de37174d1
Ben Hutchison <ben@aldaviva.com>
2025-10-21 04:36:20 -0700

    TP-Link: avoid polling for objects on webpages that aren't AX20 admin pages

--- tplink.user.js
+++ tplink.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Block TP-Link redirections
 // @namespace    https://aldaviva.com/userscripts/tplinkwifiredirect
-// @version      0.0.1
+// @version      0.0.2
 // @description  TP-Link AX20 v1.20: Don't redirect to tplinkwifi.net every time I try to log in to my router after the last session expired, because the IP address of that domain in DNS is wrong when DNS over HTTPS is enabled and the router can't intercept my DNS requests
 // @author       Ben Hutchison
 // @match        http://192.168.0.1/*
@@ -16,20 +16,24 @@
     let attempts = 0;
     const start = new Date();
 
-    const interval = setInterval(() => {
-        attempts++;
+    if(window.$ && window.$.su && window.$.su.modulManager){
+        const interval = setInterval(() => {
+            attempts++;
 
-        const main = window.$.su.moduleManager.query("main");
-        if (main) {
-            clearInterval(interval);
+            const main = window.$.su.moduleManager.query("main");
+            if (main) {
+                clearInterval(interval);
 
-            main.onLoginTimeout = () => console.info("Not redirecting to tplinkwifi.net. You're welcome!");
+                main.onLoginTimeout = () => console.info("Not redirecting to tplinkwifi.net. You're welcome!");
 
-            console.info(`Disabled onLoginTimeout so this page stops redirecting to tplinkwifi.net after ${attempts} attempts and ${new Date() - start} ms.`);
-        } else if(attempts >= 500) {
-            // give up after 10 seconds
-            clearInterval(interval);
-            console.warn(`Giving up waiting for the main module to load after ${new Date() - start} ms so redirections to tplinkwifi.net can be blocked.`);
-        }
-    }, 20);
+                console.info(`Disabled onLoginTimeout so this page stops redirecting to tplinkwifi.net after ${attempts} attempts and ${new Date() - start} ms.`);
+            } else if(attempts >= 500) {
+                // give up after 10 seconds
+                clearInterval(interval);
+                console.warn(`Giving up waiting for the main module to load after ${new Date() - start} ms so redirections to tplinkwifi.net can be blocked.`);
+            }
+        }, 20);
+    } else {
+        console.debug("Not an affected TP-Link AX20 router webpage, not fixing login page redirection bug");
+    }
 })();
